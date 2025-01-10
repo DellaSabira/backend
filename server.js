7f4a193551a8e68ea6bb9c59fcba746f61648b17
DellaSabira <dellasabira06@gmail.com>
2025-01-10 16:51:59 +0100

    init the server and migrate db

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('hello, heropay app');
+});
+
+app.listen(PORT, () =>{
+    console.log('server is running on http://localhost:${PORT}');
+});
