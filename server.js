608700c9af753a802d679b3f4937005be4cd83b9
DellaSabira <dellasabira06@gmail.com>
2025-01-10 19:08:07 +0100

    verify the db setup

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 app.use(express.json());
+
+async function main() {
+    // Fetch all users
+    const users = await prisma.user.findMany();
+    console.log('All Users:', users);
+  }
+
+  main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 app.get('/',(req,res) => {
     res.send('hello, heropay app');
